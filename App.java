df45f689a2b0d5fd79ad8daf12abe364f198ce42
root <root@ip-172-31-36-68.eu-north-1.compute.internal>
2026-08-31 09:23:05 +0000

    java-project

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App{
+	public static void main(string[] args){
+		int a=10;
+		int b=5;
+		System.out.println("Addition is:"+(a+b));
+		System.out.println("Difference is:"+(a-b));
+                System.out.println("Product is:"+(a*b));
+                System.out.println("Division is:"+(a/b));
+	}
+}
